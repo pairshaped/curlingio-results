d8aaac5e2b49ca1b76733df5d0f7d4b377344570
Dave Rapin <dave@rapin.com>
2022-10-07 13:07:50 -0400

    Added an svg for the fullscreen button

--- src/Results.elm
+++ src/Results.elm
@@ -12,6 +12,8 @@ import List.Extra
 import Process
 import RemoteData exposing (RemoteData(..), WebData)
 import RemoteData.Http
+import Svg
+import Svg.Attributes
 import Task
 import Time
 
@@ -222,6 +224,16 @@ getItems section baseUrl =
     RemoteData.Http.get url GotItems decodeItems
 
 
+svgFullScreen =
+    Svg.svg
+        [ Svg.Attributes.width "32"
+        , Svg.Attributes.height "32"
+        , Svg.Attributes.viewBox "0 0 24 24"
+        ]
+        -- [ Svg.path [ Svg.Attributes.d "M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z" ] [] ]
+        [ Svg.path [ Svg.Attributes.d "M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707zm-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707z" ] [] ]
+
+
 
 -- UPDATE
 
@@ -331,20 +343,17 @@ viewItems { flags, fullScreen } items =
     div
         [ class "p-3" ]
         [ div
-            [ class "d-flex justify-content-between mb-2" ]
+            [ class "d-flex justify-content-between" ]
             [ h5 [] [ text sectionTitle ]
-            , div [ class "text-right" ]
-                [ button [ class "btn btn-sm btn-primary mr-2", onClick ReloadItems ] [ text "Reload" ]
-                , button [ class "btn btn-sm btn-secondary", onClick ToggleFullScreen ]
-                    [ text
-                        (if fullScreen then
-                            "Exit"
 
-                         else
-                            "Full Screen"
-                        )
-                    ]
+            -- , div [ class "d-flex text-right" ]
+            -- [ button [ class "btn btn-sm btn-primary mr-2", onClick ReloadItems ] [ text "Reload" ]
+            , div
+                [ style "cursor" "pointer"
+                , style "margin-right" "-10px"
+                , onClick ToggleFullScreen
                 ]
+                [ svgFullScreen ]
             ]
         , div
             [ class "table-responsive" ]
