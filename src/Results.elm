a5fcfd76aff210cf1d07fefede4cc3375965f418
Dave Rapin <dave@rapin.com>
2022-10-12 13:00:30 -0400

    Tweaking the full screen toggle icons:

--- src/Results.elm
+++ src/Results.elm
@@ -206,22 +206,25 @@ getItems { section, baseUrl, apiKey } =
     RemoteData.Http.get url GotItems decodeItems
 
 
-svgExitFullScreen =
+
+-- SVG
+
+
+svgScreenResize path =
     Svg.svg
         [ Svg.Attributes.width "32"
         , Svg.Attributes.height "32"
         , Svg.Attributes.viewBox "0 0 24 24"
         ]
-        [ Svg.path [ Svg.Attributes.d "M.172 15.828a.5.5 0 0 0 .707 0l4.096-4.096V14.5a.5.5 0 1 0 1 0v-3.975a.5.5 0 0 0-.5-.5H1.5a.5.5 0 0 0 0 1h2.768L.172 15.121a.5.5 0 0 0 0 .707zM15.828.172a.5.5 0 0 0-.707 0l-4.096 4.096V1.5a.5.5 0 1 0-1 0v3.975a.5.5 0 0 0 .5.5H14.5a.5.5 0 0 0 0-1h-2.768L15.828.879a.5.5 0 0 0 0-.707z" ] [] ]
+        [ Svg.path [ Svg.Attributes.d path ] [] ]
+
+
+svgExitFullScreen =
+    svgScreenResize "M.172 15.828a.5.5 0 0 0 .707 0l4.096-4.096V14.5a.5.5 0 1 0 1 0v-3.975a.5.5 0 0 0-.5-.5H1.5a.5.5 0 0 0 0 1h2.768L.172 15.121a.5.5 0 0 0 0 .707zM15.828.172a.5.5 0 0 0-.707 0l-4.096 4.096V1.5a.5.5 0 1 0-1 0v3.975a.5.5 0 0 0 .5.5H14.5a.5.5 0 0 0 0-1h-2.768L15.828.879a.5.5 0 0 0 0-.707z"
 
 
 svgFullScreen =
-    Svg.svg
-        [ Svg.Attributes.width "32"
-        , Svg.Attributes.height "32"
-        , Svg.Attributes.viewBox "0 0 24 24"
-        ]
-        [ Svg.path [ Svg.Attributes.d "M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707zm0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707zm-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707z" ] [] ]
+    svgScreenResize "M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z"
 
 
 
@@ -336,6 +339,8 @@ viewItems { flags, fullScreen } items =
             [ class "d-flex justify-content-between" ]
             [ h5 [] [ text sectionTitle ]
 
+            -- I don't think we want the user initiating reloads, unless the initial load fails maybe, but even then I think
+            -- we're better off having a ticker doing reloads every 30s if they're on an event.
             -- , div [ class "d-flex text-right" ]
             -- [ button [ class "btn btn-sm btn-primary mr-2", onClick ReloadItems ] [ text "Reload" ]
             , div
